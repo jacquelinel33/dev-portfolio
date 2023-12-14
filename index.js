9780db87fee70f769813eeba945287cd5b99874f
Jacqueline Lee <jlee4332@gmail.com>
2023-12-14 16:02:39 -0500

    change to scrollY

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var nav = document.querySelector('.nav-list');
-    if (window.pageYOffset > 0) { // Check if the page is scrolled
+    if (window.scrollY > 0) { // Check if the page is scrolled
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
