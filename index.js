81b5609eea04d0e19a24f2e3670297622a0b5f48
Jacqueline Lee <jlee4332@gmail.com>
2024-01-03 20:54:06 -0500

    add domcontentloaded

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
-window.addEventListener('scroll', function() {
-    var nav = document.querySelector('.nav-list');
-    if (window.scrollY > 0) { // Check if the page is scrolled
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    // Your scroll event listener and related code here
+    window.addEventListener('scroll', function() {
+        var nav = document.querySelector('.nav-list');
+        if (window.scrollY > 0) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    });
 });
-
-
