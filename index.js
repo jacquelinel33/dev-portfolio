13cfbb76d4a39bbe98838ac8d807721f91febc0c
Jacqueline Lee <jlee4332@gmail.com>
2024-01-04 15:42:58 -0500

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Your scroll event listener and related code here
     window.addEventListener('scroll', function() {
-        var nav = document.querySelector('.nav-list');
+        var nav = document.querySelector('.nav-links');
         if (window.scrollY > 0) {
             nav.classList.add('scrolled');
         } else {
